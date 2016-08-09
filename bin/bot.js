e96d3e216ea96b1293495e94499c789dab8c144f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-08-08 22:40:54 -0700

    pass full db connection params.

--- bin/bot.js
+++ bin/bot.js
@@ -3,12 +3,20 @@
 var YoBot = require('../lib/yo-bot');
 
 var token = process.env.BOT_API_KEY;
-var dbPath = process.env.BOT_DB_PATH;
+var dbName = process.env.BOT_DB_NAME;
+var dbUser = process.env.BOT_DB_USER;
+var dbHost = process.env.BOT_DB_HOST;
+var dbPort = process.env.BOT_DB_PORT;
+var dbPass = process.env.BOT_DB_PASS;
 var name = process.env.BOT_NAME;
 
 var yoBot = new YoBot({
   token: token,
-  dbPath: dbPath,
+  dbName: dbName,
+  dbHost: dbHost,
+  dbPort: dbPort,
+  dbUser: dbUser,
+  dbPass: dbPass,
   name: name
 });
 
